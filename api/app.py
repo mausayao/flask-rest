bc57e79cab18d4a43fc76ae69e20b0af4d45adf2
mauricio <mauricio.sayao@gmail.com>
2018-07-02 20:24:52 -0400

    correção do caminho dos metodos de autenticação.

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
-from app.security import authenticate, identity
+from api.security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'segredo'

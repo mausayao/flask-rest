c6ffb8ffed95639ced4c5c34707637362be3fd07
mauricio <mauricio.sayao@gmail.com>
2018-07-02 20:05:48 -0400

    Adicionado metodos de classe para pesquisar usuario por username e id

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,43 @@
+import sqlite3
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+
+    @classmethod
+    def find_by_username(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        select = 'SELECT * FROM users WHERE username=?'
+
+        result = cursor.execute(select, (name,))
+        row = result.fetchone()
+
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        select = 'SELECT * FROM users WHERE id=?'
+
+        result = cursor.execute(select, (_id,))
+        row = result.fetchone()
+
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+
+        return user
+

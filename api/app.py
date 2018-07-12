f0c8d06637ad6f6c729fe77280c9200e68165eec
mauricio <mauricio.sayao@gmail.com>
2018-07-11 21:33:57 -0400

    Adicionado main.

--- api/app.py
+++ api/app.py
@@ -16,4 +16,6 @@ api.add_resource(ItemList, '/itens')
 
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
